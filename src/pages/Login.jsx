7f9c4bf36aab33e7c1745d36b2127e36279a7550
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-05 16:24:50 -0600

    add Link for register and login pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,10 @@
-export const Login = () => {
-  return <h1>Login page</h1>;
-};
+import { Link } from "react-router-dom";
 
+export const Login = () => {
+  return (
+    <>
+      <h1>Login page</h1>
+      <Link to="/register">Register</Link>
+    </>
+  );
+};
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,10 @@
-export const Register = () => {
-  return <h1>Register page</h1>;
-};
+import { Link } from "react-router-dom";
 
+export const Register = () => {
+  return (
+    <>
+      <h1>Register page</h1>
+      <Link to="/login">Login</Link>
+    </>
+  );
+};
