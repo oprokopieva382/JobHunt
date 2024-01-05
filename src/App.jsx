06873cb43098b7c04fc17167600f300266f7d0fb
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-05 15:35:32 -0600

    create skeleton for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-export const App=()=> {
-  return (
-    <div>JobHunt</div>
-  )
-}
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>home</h1>,
+  },
+  {
+    path: "/about",
+    element: (
+      <div>
+        <h2>about page</h2>
+      </div>
+    ),
+  },
+]);
+
+export const App = () => {
+  return <RouterProvider router={router} />;
+};
