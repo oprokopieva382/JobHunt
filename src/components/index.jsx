ef40c918fb918efcc86d3a711b41ffa29d206593
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-06 22:28:37 -0600

    create register form with style

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,1 +1,2 @@
 export { Logo } from "./logo";
+export { FormRow } from "./FormRow";
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import {FormRow} from "../components";
 import { Logo } from "../components";
 
 export const Register = () => {
@@ -8,11 +9,16 @@ export const Register = () => {
       <form className="form">
         <Logo />
         <h4>Register</h4>
+        <FormRow name="name" defaultValue="John" type="text"/>
+        <FormRow name="lastName" labelText="Last Name" defaultValue="Smith" type="text"/>
+        <FormRow name="location" defaultValue="Moon" type="text"/>
+        <FormRow name="email" defaultValue="John@gmail.com" type="text"/>
+        <FormRow name="password" defaultValue="John123" type="password"/>
         <button type="submit" className="btn btn-block">
           submit
         </button>
         <p>
-          Already a member?{" "}
+          Already a member?
           <Link to="/login" className="member-btn">
             Login
           </Link>
