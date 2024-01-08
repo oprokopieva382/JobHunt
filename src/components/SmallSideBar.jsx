6cc110db5a0dd8d5872607b4ac5f82cd4ea8a926
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-08 16:13:40 -0600

    create reusable NavLinks to keep code DRY and use components in Smal and Big sidebar

--- src/components/SmallSideBar.jsx
+++ src/components/SmallSideBar.jsx
@@ -1,16 +1,19 @@
 import { FaTimes } from "react-icons/fa";
 import Wrapper from "../assets/wrappers/SmallSidebar";
 import { Logo } from "./logo";
-import { links } from "../utils/Links";
-import { NavLink } from "react-router-dom";
 import { useDashboardContext } from "../hooks/useDashboardContext";
+import { NavLinks } from "./NavLinks";
 
 export const SmallSideBar = () => {
   const { showSideBar, toggleSideBar } = useDashboardContext();
- 
+
   return (
     <Wrapper>
-      <div className={showSideBar ? "sidebar-container show-sidebar" : "sidebar-container"}>
+      <div
+        className={
+          showSideBar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
         <div className="content">
           <button type="button" className="close-btn" onClick={toggleSideBar}>
             <FaTimes />
@@ -18,23 +21,7 @@ export const SmallSideBar = () => {
           <header className="navbar-logo">
             <Logo />
           </header>
-          <div className="nav-links">
-            {links.map((link) => {
-              const { text, path, icon } = link;
-              return (
-                <NavLink
-                  to={path}
-                  key={text}
-                  className="nav-link"
-                  onClick={toggleSideBar}
-                  end
-                >
-                  <span className="icon">{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
-          </div>
+          <NavLinks />
         </div>
       </div>
     </Wrapper>
