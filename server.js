5a6b4e4e57c10d0c7614ae5f75edc7acfb3df2a6
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-09 18:09:50 -0600

    connect port 5100

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("server running...")
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello server");
+});
+
+app.listen(5100, () => {
+  console.log("listen on the port 5100");
+});
