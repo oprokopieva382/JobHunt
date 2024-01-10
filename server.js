83e408ff2293f5281e228f55976104e1a6c017c6
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-09 18:24:47 -0600

    connect middleware to accept json

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello server");
+});
+
+app.post("/", (req, res) => {
+  res.json({ message: "data received", data: req.body });
 });
 
 app.listen(5100, () => {
