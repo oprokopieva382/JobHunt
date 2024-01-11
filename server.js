52e23f3f74c6d8d0a864d46e2d1dd701d9ee45a1
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-10 22:51:07 -0600

    create get request for single job

--- server.js
+++ server.js
@@ -41,7 +41,19 @@ app.post("/api/v1/jobs", (req, res) => {
   const id = nanoid(10);
   const job = { id, company, position };
   jobs.push(job)
-  res.status(200).json({ job });
+  res.status(201).json({ job });
+});
+
+//GET SINGLE JOB
+app.get("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job)=> job.id === id) 
+  if (!job) {
+    return res
+      .status(404)
+      .json({ error: `no job with such ${id}` });
+  }
+ res.status(200).json({job})
 });
 
 const PORT = process.env.PORT || 5100;
