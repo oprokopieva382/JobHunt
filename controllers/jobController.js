8ca1140b4da899926c02188365cd69fac3f45f9a
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-11 10:36:05 -0600

    move createJob to controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -8,3 +8,16 @@ let jobs = [
 export const allJobs = async (req, res) => {
   res.status(200).json({ jobs });
 };
+
+export const createJob = async (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res
+      .status(400)
+      .json({ error: "please provide company and position" });
+  }
+  const id = nanoid(10);
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(201).json({ job });
+};
--- server.js
+++ server.js
@@ -29,18 +29,7 @@ app.post("/", (req, res) => {
 app.get("/api/v1/jobs", );
 
 //CREATE JOB
-app.post("/api/v1/jobs", (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res
-      .status(400)
-      .json({ error: "please provide company and position" });
-  }
-  const id = nanoid(10);
-  const job = { id, company, position };
-  jobs.push(job);
-  res.status(201).json({ job });
-});
+app.post("/api/v1/jobs", );
 
 //GET SINGLE JOB
 app.get("/api/v1/jobs/:id", (req, res) => {
