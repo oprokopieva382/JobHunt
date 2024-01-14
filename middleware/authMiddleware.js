77737d374a8939a2497e609cb39518bf501d1214
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-14 17:33:26 -0600

    create verifyJWT and modify authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,15 @@
 import { UnauthenticatedError } from "../errors/customError.js";
+import { verifyJWT } from "../utils/tokenUtils.js";
 
 export const authMiddleware = (req, res, next) => {
   const { token } = req.cookies;
   if (!token) throw new UnauthenticatedError("unauthenticated error");
-  console.log(token);
-  next();
+
+  try {
+    const { userId, role } = verifyJWT(token);
+    req.user = { userId, role };
+    next();
+  } catch (err) {
+    throw new UnauthenticatedError("unauthenticated error");
+  }
 };
