0430e8dab13bcbf3b1028212cb94a3b67d5eecb0
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-14 17:22:43 -0600

    verify cookie in authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,8 @@
+import { UnauthenticatedError } from "../errors/customError.js";
+
 export const authMiddleware = (req, res, next) => {
-  console.log("auth middleware");
+  const { token } = req.cookies;
+  if (!token) throw UnauthenticatedError("unauthenticated error");
+  console.log(token);
   next();
 };
