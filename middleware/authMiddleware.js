d52a65ed9afc1553a65b7590ad97d19d0d3aaf53
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-14 17:25:02 -0600

    fix bug in authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ import { UnauthenticatedError } from "../errors/customError.js";
 
 export const authMiddleware = (req, res, next) => {
   const { token } = req.cookies;
-  if (!token) throw UnauthenticatedError("unauthenticated error");
+  if (!token) throw new UnauthenticatedError("unauthenticated error");
   console.log(token);
   next();
 };
