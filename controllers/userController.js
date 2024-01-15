2251b3e3433fa6d6633fed4bcb98c0a76e49e942
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-14 19:02:39 -0600

    create start code for userController and make userRouter

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,16 @@
+import { StatusCodes } from "http-status-codes"
 
+export const getCurrentUser = (req, res)=> {
+res.status(StatusCodes.OK).json({msg: "current user route"})
+}
+
+export const getApplicationStats = (req, res) => {
+  res.status(StatusCodes.OK).json({ msg: "application stats" });
+};
+
+export const updateCurrentUser = (req, res) => {
+  res.status(StatusCodes.OK).json({ msg: "user updated" });
+};
 
 //DELETE USER
 // export const deleteUser = async (req, res) => {
