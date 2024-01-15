269d8cda01f964bce8ec99694f9a36e51616b767
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-14 18:47:55 -0600

    create user to logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,6 +37,14 @@ export const login = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: "user logged in" });
 };
 
+export const logout = (req, res) => {
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCodes.OK).json({msg: "user logged out"})
+};
+
 //DELETE USER
 // export const deleteUser = async (req, res) => {
 //   const { id } = req.params;
