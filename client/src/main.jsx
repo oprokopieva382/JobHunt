e68a4b979c50a9574fbf8fb6bdaaff690a60fc42
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-14 23:49:19 -0600

    set up axios to fetch the data and create axios instance

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
+import { customFetch } from "./utils/customFetch.js";
 
-fetch("/api/v1/test")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+const res = await customFetch.get("test")
+console.log(res);
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
