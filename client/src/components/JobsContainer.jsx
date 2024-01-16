bd2aa4623712ef9bd817b27d52d8f564150deb1f
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-16 11:55:04 -0600

    make jobContainer to display job dynamic but hardcode yet

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,4 +1,24 @@
-export const JobsContainer = () => {
-    return <div>JobsContainer</div>;
-}
+import Wrapper from "../assets/wrappers/JobsContainer";
+import { useAllJobsContext } from "./../hooks/useAllJobsContext";
+import { Job } from "./Job";
 
+export const JobsContainer = () => {
+  const { data } = useAllJobsContext();
+
+  if (data.jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs yet...</h2>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <div className="jobs">
+        {data.jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
+};
