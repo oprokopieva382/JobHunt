afb8c4b37e2a25f12a0a7b4672fab12bf572fd2e
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-16 10:20:45 -0600

    create start code for AddJob page

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,7 +1,38 @@
-import { useDashboardContext } from "../hooks/useDashboardContext";
+import { FormRow } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useOutletContext } from "react-router-dom";
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
+import { Form, useNavigation, redirect } from "react-router-dom";
+import { toast } from "react-toastify";
+import {customFetch} from "../utils/customFetch";
 
 export const AddJob = () => {
-  const data = useDashboardContext();
-  console.log(data);
-  return <h1>AddJob page</h1>;
+  const { user } = useOutletContext();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  return (
+    <Wrapper>
+      <Form method="post" className="form">
+        <h4 className="form-title">add job</h4>
+        <div className="form-center">
+          <FormRow type="text" name="position" />
+          <FormRow type="text" name="company" />
+          <FormRow
+            type="text"
+            name="jobLocation"
+            labelText="job location"
+            defaultValue={user.location}
+          />
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="btn btn-block form-btn"
+          >
+            {isSubmitting ? "Submitting..." : "Submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
