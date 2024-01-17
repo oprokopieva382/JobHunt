abfd6b2171370ed3682c2ced66e414a6dcfac47b
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-17 00:21:54 -0600

    stats for users and jobs

---
+++ client/src/components/StatCard.jsx
@@ -0,0 +1,13 @@
+import Wrapper from "../assets/wrappers/StatCard";
+
+export const StatCard = ({ count, color, bcg, title, icon }) => {
+  return (
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className="count">{count}</span>
+        <span className="icon">{icon}</span>
+      </header>
+      <h5 className="title">{title}</h5>
+    </Wrapper>
+  );
+};
--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,7 +1,10 @@
 import { useLoaderData, redirect } from "react-router-dom";
 import { customFetch } from "../utils/customFetch";
 import Wrapper from "../assets/wrappers/StatsContainer";
+import { FaUsers } from "react-icons/fa";
+import { LiaClipboardListSolid } from "react-icons/lia";
 import { toast } from "react-toastify";
+import { StatCard } from "../components";
 
 export const loader = async () => {
   try {
@@ -14,10 +17,23 @@ export const loader = async () => {
 };
 
 export const Admin = () => {
-  const { user, job } = useLoaderData();
+  const { users, jobs } = useLoaderData();
   return (
     <Wrapper>
-      <h2>admin page</h2>
+      <StatCard
+        title="current user"
+        count={users}
+        color="#e9b949"
+        bcg="#fcefc7"
+        icon={<FaUsers />}
+      />
+      <StatCard
+        title="current jobs"
+        count={jobs}
+        color="#647acb"
+        bcg="#e0e8f9"
+        icon={<LiaClipboardListSolid />}
+      />
     </Wrapper>
   );
 };
