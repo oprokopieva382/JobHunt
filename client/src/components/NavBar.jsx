e1b22a1567bc40942ec695ce4cab867f086745fd
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-17 11:42:01 -0600

    set up public folder for uploaded pics

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -16,11 +16,11 @@ export const NavBar = () => {
         </button>
         <div className="navbar-logo">
           <Logo />
-          <h4 className="logo-text">dashboard</h4>
+          <h4 className="logo-text">Career Dashboard</h4>
         </div>
         <div className="btn-container">
-        <ThemeToggle/>
-        <LogoutContainer/>
+          <ThemeToggle />
+          <LogoutContainer />
         </div>
       </div>
     </Wrapper>
