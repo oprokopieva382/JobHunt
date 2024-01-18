fd9ed734a2ae1081c2d8693afb46b76ab53406bb
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-18 10:56:34 -0600

    set up logic for app test drive

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { Form, redirect, Link } from "react-router-dom";
+import { Form, redirect, Link, useNavigate } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, SuperSubmitButton } from "../components";
 import { Logo } from "../components";
@@ -8,7 +8,7 @@ import { customFetch } from "../utils/customFetch";
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-   try {
+  try {
     await customFetch.post("auth/login", data);
     toast.success("Login successful");
     return redirect("/dashboard");
@@ -19,6 +19,21 @@ export const action = async ({ request }) => {
 };
 
 export const Login = () => {
+  const navigate = useNavigate();
+
+  const userTestDrive = async () => {
+    const testData = {
+      email: "test@test.com",
+      password: "secret123",
+    };
+    try {
+      await customFetch.post("auth/login", testData);
+      toast.success("Explore The JobHunt App");
+      return navigate("/dashboard");
+    } catch (err) {
+      toast.error(err?.response?.data?.msg);
+    }
+  };
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -26,7 +41,7 @@ export const Login = () => {
         <FormRow name="email" defaultValue="John@gmail.com" type="email" />
         <FormRow name="password" defaultValue="John123" type="password" />
         <SuperSubmitButton />
-        <button type="button" className="btn btn-block">
+        <button type="button" className="btn btn-block" onClick={userTestDrive}>
           explore the app
         </button>
         <p>
