af8ebfad122139f150b554d9afc741e6b3a546ea
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-24 11:54:03 -0600

    set up pages for pagination to display dynamic

--- client/src/components/PaginationContainer.jsx
+++ client/src/components/PaginationContainer.jsx
@@ -1,14 +1,36 @@
 import Wrapper from "../assets/wrappers/PaginationContainer.js";
 import { useAllJobsContext } from "../hooks/useAllJobsContext.jsx";
+import {
+  TbPlayerTrackPrevFilled,
+  TbPlayerTrackNextFilled,
+} from "react-icons/tb";
 
 export const PaginationContainer = () => {
   const {
     data: { currentPage, numOfPages },
   } = useAllJobsContext();
+  const pages = Array.from({ length: numOfPages }, (_, i) => i + 1);
 
   return (
     <Wrapper>
-      {currentPage} {numOfPages}
+      <button className="btn prev-btn">
+        <TbPlayerTrackPrevFilled />
+        PREV
+      </button>
+      <div className="btn-container">
+        {pages.map((page) =>  (
+            <button
+              key={page}
+              className={`btn page-btn ${page === currentPage && "active"}`}
+            >
+              {page}
+            </button>
+          ))}
+      </div>
+      <button className="btn next-btn">
+        NEXT
+        <TbPlayerTrackNextFilled />
+      </button>
     </Wrapper>
   );
 };
