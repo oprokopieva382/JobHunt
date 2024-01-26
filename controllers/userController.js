26ca69671d857665e6d2a54cae7eecd0650d6ee4
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-25 23:26:47 -0600

    update in userController.js updateCurrentUser functionality so that I can fix image upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,8 @@
 import cloudinary from "cloudinary";
-import { promises as fs } from "fs";
 import { StatusCodes } from "http-status-codes";
 import Job from "../models/JobModel.js";
 import User from "../models/UserModel.js";
+import imageFormat from "../middleware/multerMiddleware.js";
 
 //GET CURRENT USER
 export const getCurrentUser = async (req, res) => {
@@ -22,8 +22,9 @@ export const updateCurrentUser = async (req, res) => {
   const newUser = { ...req.body };
   delete newUser.password;
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = imageFormat(req.file)
+    const response = await cloudinary.v2.uploader.upload(file);
+   
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
