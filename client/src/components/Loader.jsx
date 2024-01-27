72c20860ac76e462b2fc100cf86dbedeed0eeb86
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-01-26 22:33:30 -0600

    set up loader style

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,15 +1,17 @@
 import { BallTriangle } from "react-loader-spinner";
+import Wrapper from "../assets/wrappers/Loader";
 
 export const Loader = () => {
   return (
-    <BallTriangle
-      height={100}
-      width={100}
-      radius={5}
-      color="#af6f39"
-      ariaLabel="ball-triangle-loading"
-      wrapperClass=""
-      visible={true}
-    />
+    <Wrapper>
+      <BallTriangle
+        height={100}
+        width={100}
+        radius={5}
+        color="#af6f39"
+        ariaLabel="ball-triangle-loading"
+        visible={true}
+      />
+    </Wrapper>
   );
 };
